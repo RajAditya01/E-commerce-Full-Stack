e31af559228dfc3e674893169ca61dac497fc1f5
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-05-09 12:05:12 +0530

    Header+search

---
+++ my-app/components/SearchResult.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchResult = () => {
+    return (
+        <div>
+            {/* Your JSX content for the search result */}
+        </div>
+    );
+}
+
+export default SearchResult;
