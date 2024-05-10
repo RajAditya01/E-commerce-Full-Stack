7bb4699f821ebf018bb901b783c65c3dfc19cd8f
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-05-10 12:52:39 +0530

    rating

--- my-app/components/ProductCard.tsx
+++ my-app/components/ProductCard.tsx
@@ -1,9 +1,20 @@
 import react from "react";
+import Image from "next/image";
 
-const ProductCard = ()=>{
+const ProductCard = ({product:any})=>{
     return(
-        <>
-        </>
+        <div><div>
+            <div className="bg-gray-100 h-fit">
+                <Image className="mix-blend-multiply" src={product.title} alt={product.title} width={250} height={400} />
+            </div>
+            <h1 className="font-bold">{product.title}</h1>
+            <p>{`${products.description.substring(0,50)}...`} </p>
+            <p className="font-bold text-2xl"> {`$${product.price}`}
+
+            </p>
+        </div>
+            <Image src={products.image} alt={product.title} width={250} height={400} />
+        </div>
     )
 }
 
